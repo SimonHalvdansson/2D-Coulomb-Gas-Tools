15117f3f3378c8eb6a5d81330e1f64c9fd7ff2db
Simon Halvdansson <simonhalvdansson@gmail.com>
2023-10-15 21:51:52 +0200

    Added back all potentials

--- worker.js
+++ worker.js
@@ -7,22 +7,41 @@ self.addEventListener('message', function(e) {
   const h = e.data.h;
   const dt = e.data.dt;
   const pot = e.data.pot;
+  const lemniscateT = e.data.lemniscateT;
+  const lemInterpol = e.data.lemInterpol;
+  const ca = e.data.ca;
+  const cb = e.data.cb;
+  const bx = e.data.bx;
+  const by = e.data.by;
+  const ax = e.data.ax;
+  const ay = e.data.ay;
 
   function Q(x, y) {
-    q = 0
+    let q = 0;
+    let real = x;
+    let imag = y;
 
-    //Ginibre
-    if (pot == 0)
-      q += x*x+y*y
-    //Mittag-Leffler
-    if (pot == 1)
-      q += Math.pow(x*x+y*y, 2)
-    //mittag-leffler lambda=10
-    if (pot == 2)
-      q += Math.pow(x*x+y*y, 10)
+    let r = Math.sqrt(real * real + imag * imag);
+    let theta = Math.atan2(imag, real);
 
-    return q
-    
+    if (pot === 0)
+      q += x * x + y * y;
+    if (pot === 1)
+      q += Math.pow(x * x + y * y, 2);
+    if (pot === 2)
+      q += Math.pow(x * x + y * y, 10);
+    if (pot === 3)
+      q += Math.pow(x * x + y * y, 2) - lemniscateT * 2 / Math.sqrt(2) * Math.pow(r, 2) * Math.cos(2 * theta);
+    if (pot === 4)
+      q += Math.pow(x * x + y * y, 3) - lemniscateT * 2 / Math.sqrt(3) * Math.pow(r, 3) * Math.cos(3 * theta);
+    if (pot === 5)
+      q += Math.pow(x * x + y * y, 3) - lemniscateT * 2 / Math.sqrt(5) * Math.pow(r, 5) * Math.cos(5 * theta);
+    if (pot === 6)
+      q += Math.pow(x * x + y * y, lemInterpol) - lemniscateT * 2 / Math.sqrt(lemInterpol) * Math.pow(r, lemInterpol) * Math.cos(lemInterpol * theta);
+
+    q += (-cb * Math.log(Math.sqrt(Math.pow(x - bx, 2) + Math.pow(y - by, 2))) - ca * Math.log(Math.sqrt(Math.pow(x - ax, 2) + Math.pow(y + ay, 2))));
+
+    return q;
   }
 
   //first do the Q derivative numerically (as it is not O(n^2))
